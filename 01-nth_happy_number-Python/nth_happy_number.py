f5071a89e3806e10b1e3a56abb63c073e9557c98
lakshmisindhu9100733271 <lakshmisindhu996@msitprogram.net>
2020-07-21 13:20:14 +0530

    Done

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -11,5 +11,23 @@
 # assert(nthHappyNumber(7) == 31)
 
 
+def ishappynumber(n):
+	if n==1:
+		return True
+	elif n<=0 or n==4:
+		return False
+	else:
+		s=0
+		rem=0
+		while(n>0):
+			rem=n%10
+			s=s+(rem)**2
+			n=n//10
+		return ishappynumber(s)
 def fun_nth_happy_number(n):
-	return 0
+	c=1
+	while n>=0:
+		if ishappynumber(c):
+			n-=1
+		c+=1
+	return c-1
