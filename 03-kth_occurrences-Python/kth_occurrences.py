e2f7474df1edd1f451f1e8fbdf6bc7db77d1c11a
lakshmisindhu9100733271 <lakshmisindhu996@msitprogram.net>
2020-07-22 19:56:09 +0530

    Done

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,19 @@
 
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	s=s.strip()
+	if len(s)==1:
+		return s
+	else:
+		l=[]
+		for i in s:
+			 if (s.conut(i),i) not in l and i!='':
+				l.append((s.count(i),i))
+		l.sort(key=lambda x:x[0],reverse=True)
+		try:
+			return l[n-1][1]
+		except:
+			return l[0][1]
+	
 
 
