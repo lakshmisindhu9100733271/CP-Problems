c4f82c7090cee29aab445f336848d39b0ce6383f
lakshmisindhu9100733271 <lakshmisindhu996@msitprogram.net>
2020-07-25 14:33:24 +0530

    Done

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -5,6 +5,26 @@
 # fun_nth_tidynumber(5) = 6
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
+def small(n):
+    if n<10:
+        return True
+    else:
+        while(n!=0):
+            r=n%10
+            n=n//10
+            re=n%10
+            if re>r:
+                return False
+        return True
 
 def fun_nth_tidynumber(n):
-    return 0
+    c=0
+    i=1
+    while True:
+        if small(i):
+            if c==n:
+                break
+            c+=1
+        i+=1
+    return i        
+
